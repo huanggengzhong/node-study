c3df2029fa6079f639cde695e8824c0c37d0e56c
huanggengzhong <915835528@qq.com>
2021-03-05 23:46:14 +0800

    备份

--- 12-mysql/01-语句学习/02-语句学习(表操作).sql
+++ 12-mysql/01-语句学习/02-语句学习(表操作).sql
@@ -9,9 +9,23 @@ DESC users;
 # 查看创建表结构的完整语句
 SHOW CREATE TABLE `users`;
 
-# 完整的创建表
+# 创建表,VARCHAR代表唯一性,NOT NULL代表不能为空
 
-CREATE TABLE IF NOT EXISTS users(name VARCHAR(20),age INT ,height DOUBLE);
+-- CREATE TABLE IF NOT EXISTS users(name VARCHAR(20),age INT ,height DOUBLE,phone VARCHAR(20) );
+-- 完整的创建
+-- primary key主键,auto_increment自动递增,UNIQUE不能重复
+CREATE TABLE IF NOT EXISTS `students` (
+id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(20) NOT NULL,
+age INT DEFAULT 0,
+phoneNum VARCHAR(20) UNIQUE DEFAULT '',
+createTime TIMESTAMP
+);
+
 
 # 删除表
-DROP TABLE IF EXISTS users;
+DROP TABLE students;
+DROP TABLE IF EXISTS students;
+
+# 修改表名
+ALTER TABLE `users` RENAME TO `user`;
